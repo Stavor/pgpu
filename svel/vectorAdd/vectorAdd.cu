44e0f34b245db3847bb75176ba745c9d3eafd05e
unknown <denis.boginich@nv-ur-121.kontur>
2014-10-30 21:42:00 +0500

    Запускаю последовательно куда ядра с/без использования shared памяти. Замеряю и вывожу время выполнения.

--- svel/vectorAdd/vectorAdd.cu
+++ svel/vectorAdd/vectorAdd.cu
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <functional>
 
-static int threadsPerBlock = 256;
+using std::tr1::function;
+
+static const int threadsPerBlock = 256;
 
 __global__ void reductionByAdd(int *a, int *res, int elCnt) {
 	int firstThreadBlockId = blockIdx.x * blockDim.x;	
@@ -17,26 +20,62 @@ __global__ void reductionByAdd(int *a, int *res, int elCnt) {
 		res[blockIdx.x] = a[firstThreadBlockId];
 }
 
+__global__ void reductionByAddWithShared(int *a, int *res, int elCnt) {
+	extern __shared__ int sdata[];
+
+	int threadId = threadIdx.x;
+	int glThreadId = blockIdx.x * blockDim.x + threadIdx.x;
+
+	sdata[threadId] = glThreadId < elCnt ? a[glThreadId] : 0;
+	__syncthreads();
+
+	for (int i = 1; i < blockDim.x; i <<= 1) {
+		if(threadId % (2 * i) == 0 && threadId + i < blockDim.x) {
+			sdata[threadId] += sdata[threadId + i];
+		}
+		__syncthreads();
+	}
+	
+	if(threadId == 0)
+		res[blockIdx.x] = sdata[0];
+}
+
 void checkCudaError(cudaError error) {
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Failed! (error cdoe %s)\n", cudaGetErrorString(error));
+		fprintf(stderr, "Failed! (error code %s)\n", cudaGetErrorString(error));
 		exit(EXIT_FAILURE);
 	}
 }
 
-__host__ void reductionAdd(int *a, int *res, int elCnt) {
+__host__ void recursiveAddReduction(int *a, int *res, int elCnt, function<void (int, int, int *, int *n, int)> reductionImpl) {
 	int blocksPerGrid = (elCnt + threadsPerBlock - 1) / threadsPerBlock;
 	size_t sizeRes = blocksPerGrid * sizeof(int);
 	
+	//printf("%d\n", blocksPerGrid);
+
 	int *result = NULL;
 	cudaError_t error = cudaMalloc((void**)&result, sizeRes);
 	checkCudaError(error);
 
-	reductionByAdd<<<blocksPerGrid, threadsPerBlock>>>(a, result, elCnt);
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	float time;
+
+	cudaEventRecord(start, 0);
+
+	reductionImpl(blocksPerGrid, threadsPerBlock, a, result, elCnt);
+	
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&time, start, stop);
+	printf("Time is : %f\n", time);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 	if(elCnt > threadsPerBlock) {
-		reductionAdd(result, res, blocksPerGrid);
-		checkCudaError(error);
+		recursiveAddReduction(result, res, blocksPerGrid, reductionImpl);
 	} else {
 		error = cudaMemcpy(res, result, sizeof(int), cudaMemcpyDeviceToHost);
 		checkCudaError(error);
@@ -44,18 +83,38 @@ __host__ void reductionAdd(int *a, int *res, int elCnt) {
 
 	error =  cudaFree(result);
 	checkCudaError(error);
+
+    error = cudaGetLastError();
+    checkCudaError(error);
+}
+
+void resultVerification(int redRes, int *m, int cnt) {
+	int rightRes = 0;
+    for (int i = 0; i < cnt; ++i) {
+    	rightRes += m[i];
+    }
+
+    printf("%d\n", rightRes);
+    printf("%d\n", redRes);
+    int abSub = abs(rightRes - redRes);
+    printf("%d\n", abSub);
+
+    if (abSub != 0) {
+            fprintf(stderr, "Result verification failed!\n");
+            exit(EXIT_FAILURE);
+    }
 }
 
 int main() {
-	int elementCnt = 7000000;
+	int elementCnt = 400000; //4M
 
-	size_t size1 = elementCnt * sizeof(int);
+	size_t size = elementCnt * sizeof(int);
 	
-	int *hosta = (int *)malloc(size1);
+	int *hosta = (int *)malloc(size);
 	int *ans = (int *)malloc(sizeof(int));
 
-	if(hosta == NULL) {
-		fprintf(stderr, "Failed to allocate host vectors!\n");
+	if(hosta == NULL || ans == NULL) {
+		fprintf(stderr, "Failed to allocate host data!\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -63,33 +122,30 @@ int main() {
 		hosta[i] = rand() % 100;
 	}
 
-	int *deva1 = NULL;
-	cudaError_t error = cudaMalloc((void**)&deva1, size1);
+	int *deva = NULL;
+	cudaError_t error = cudaMalloc((void**)&deva, size);
 	checkCudaError(error);
 
-	error = cudaMemcpy(deva1, hosta, size1, cudaMemcpyHostToDevice);
+	error = cudaMemcpy(deva, hosta, size, cudaMemcpyHostToDevice);
 	checkCudaError(error);
 
-    reductionAdd(deva1, ans, elementCnt);
+	printf("With Shared mem:\n");
+	recursiveAddReduction(deva, ans, elementCnt, [] (int blocksPerGrid, int threadsPerBlock, int *a, int *res, int elCnt) -> void
+		{
+			reductionByAddWithShared<<<blocksPerGrid, threadsPerBlock, threadsPerBlock*sizeof(int)>>>(a, res, elCnt);
+		});
+    resultVerification(*ans, hosta, elementCnt);
+    
+    *ans = -1;
 
-    error = cudaGetLastError();
-    checkCudaError(error);
+    printf("\nOnly globalname:\n");
+    recursiveAddReduction(deva, ans, elementCnt, [] (int blocksPerGrid, int threadsPerBlock, int *a, int *res, int elCnt) -> void
+		{
+			reductionByAdd<<<blocksPerGrid, threadsPerBlock>>>(a, res, elCnt);
+		});
+    resultVerification(*ans, hosta, elementCnt);
 
-    //Check
-    int res = 0;
-    for (int i = 0; i < elementCnt; ++i) {
-    	res += hosta[i];
-    }
-    printf("%d\n", res);
-    printf("%d\n", *ans);
-    printf("%d\n", abs(res - *ans));
-
-    if (abs(res - *ans) != 0) {
-            fprintf(stderr, "Result verification failed!\n");
-            exit(EXIT_FAILURE);
-    }
-
-    error = cudaFree(deva1);
+    error = cudaFree(deva);
 	checkCudaError(error);
 
     free(hosta);
